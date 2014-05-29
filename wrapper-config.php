b9fa9a65e16932127cb6c733994e0f05cfaf1f01
Richiearnold <richard@ambercouch.co.uk>
2014-05-29 11:15:34 +0100

    Added uploads directory

--- wrapper-config.php
+++ wrapper-config.php
@@ -17,3 +17,6 @@ if (!defined('WP_CONTENT_DIR')) {
 if (!defined('WP_CONTENT_URL')) {
   define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/content');
 }
+if (!defined('WP_CONTENT_URL')) {
+  define('UPLOADS', 'http://' . $_SERVER['SERVER_NAME'] . '/content/uploads');
+}
