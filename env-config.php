2021ba67ee08bc3799d775d9b178fc0244669aa3
Richiearnold <richard@ambercouch.co.uk>
2018-04-24 13:24:42 +0100

    Add environment arrays

--- env-config.php
+++ env-config.php
@@ -3,16 +3,33 @@
 // Define Environments
 $environments = array(
     'local' => '.local',
-    'development' => 'dev.'
+    'development' => ['.test', '.dev','.sta'],
+    'production' => ['.prod','.live']
 );
 // Get Server name
 $server_name = $_SERVER['SERVER_NAME'];
 
+function server_name_env($env, $server_name){
+    if (strstr($server_name, $env)) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
 foreach ($environments AS $key => $env) {
-  if (strstr($server_name, $env)) {
-    define('ENVIRONMENT', $key);
-    break;
-  }
+
+    $env_array = (is_array($env)) ? $env : [$env];
+
+    foreach ($env_array as $env){
+
+        if(server_name_env($env, $server_name)){
+            define('ENVIRONMENT', $key);
+            break;
+        };
+
+    }
+
 }
 
 // If no environment is set default to production
