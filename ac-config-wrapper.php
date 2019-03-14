ead57cef9edd0bbfafde06d96703aa4fee2f7ac4
Richiearnold <richard@ambercouch.co.uk>
2019-03-14 19:34:26 +0000

    Add protocol to custom path urls

--- ac-config-wrapper.php
+++ ac-config-wrapper.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Test ssl / https
+ */
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 
 /**
  * Set custom paths
@@ -7,16 +11,16 @@
  * These are required because wordpress is installed in a subdirectory.
  */
 if (!defined('WP_SITEURL')) {
-  define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/cms');
+  define('WP_SITEURL', $protocol . $_SERVER['SERVER_NAME'] . '/cms');
 }
 if (!defined('WP_HOME')) {
-  define('WP_HOME', 'http://' . $_SERVER['SERVER_NAME'] . '');
+  define('WP_HOME', $protocol . $_SERVER['SERVER_NAME'] . '');
 }
 if (!defined('WP_CONTENT_DIR')) {
   define('WP_CONTENT_DIR', dirname(__FILE__) . ACT_CONTENT);
 }
 if (!defined('WP_CONTENT_URL')) {
-  define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . ACT_CONTENT);
+  define('WP_CONTENT_URL', $protocol . $_SERVER['SERVER_NAME'] . ACT_CONTENT);
 }
 if (!defined('UPLOADS')) {
   define('UPLOADS', '..'.ACT_CONTENT.'/uploads');
