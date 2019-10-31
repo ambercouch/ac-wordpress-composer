b9f4b6331eb945e91387397cd75014b3bdb53ace
Richie Arnold <richard@ambercouch.co.uk>
2019-10-31 13:11:35 +0000

    Add php level www. suppression

--- ac-config-wrapper.php
+++ ac-config-wrapper.php
@@ -5,22 +5,28 @@
  */
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 
+$server_name = $_SERVER['SERVER_NAME'];
+
+if (substr( $server_name, 0, 4) === "www."){
+    $server_name = ltrim($server_name, 'www.');
+}
+
 /**
  * Set custom paths
  *
  * These are required because wordpress is installed in a subdirectory.
  */
 if (!defined('WP_SITEURL')) {
-  define('WP_SITEURL', $protocol . $_SERVER['SERVER_NAME'] . '/cms');
+  define('WP_SITEURL', $protocol . $server_name  . '/cms');
 }
 if (!defined('WP_HOME')) {
-  define('WP_HOME', $protocol . $_SERVER['SERVER_NAME'] . '');
+  define('WP_HOME', $protocol . $server_name  . '');
 }
 if (!defined('WP_CONTENT_DIR')) {
   define('WP_CONTENT_DIR', dirname(__FILE__) . ACT_CONTENT);
 }
 if (!defined('WP_CONTENT_URL')) {
-  define('WP_CONTENT_URL', $protocol . $_SERVER['SERVER_NAME'] . ACT_CONTENT);
+  define('WP_CONTENT_URL', $protocol . $server_name  . ACT_CONTENT);
 }
 if (!defined('UPLOADS')) {
   define('UPLOADS', '..'.ACT_CONTENT.'/uploads');
